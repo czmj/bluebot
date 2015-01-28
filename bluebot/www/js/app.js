f723188757a931ec25163b4676e45d8c3e140cde
czmj <clara@jordan-maynard.org>
2015-01-28 22:58:09 +0000

    static highcharts working

--- bluebot/www/js/app.js
+++ bluebot/www/js/app.js
@@ -5,7 +5,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'highcharts-ng'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -35,7 +35,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     url: "/graph",
     views: {
       'menuContent': {
-        templateUrl: "templates/graph.html"
+        templateUrl: "templates/graph.html",
       }
     }
   })
