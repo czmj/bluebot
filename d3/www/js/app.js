a0a4d8b5de87efc868d9aa6e9ec47bb683f2ac65
czmj <clara@jordan-maynard.org>
2015-01-28 14:28:56 +0000

    d3 chart

---
+++ d3/www/js/app.js
@@ -0,0 +1,33 @@
+(function () {
+
+    "use strict";
+
+    document.addEventListener("deviceready", function () {
+        FastClick.attach(document.body);
+        StatusBar.overlaysWebView(false);
+    }, false);
+
+
+    // Show/hide menu toggle
+    $('#btn-menu').click(function () {
+        if ($('#container').hasClass('offset')) {
+            $('#container').removeClass('offset');
+        } else {
+            $('#container').addClass('offset');
+        }
+        return false;
+    });
+
+    // Basic view routing
+    $(window).on('hashchange', route);
+
+    function route() {
+        var hash = window.location.hash;
+        if (hash === "#dashboard/1") {
+            graph.render();
+        }
+    }
+
+    graph.render();
+
+}());
